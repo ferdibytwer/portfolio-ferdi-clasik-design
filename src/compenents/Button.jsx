d9387e4b43faa0a6713182327e6e09b001cfa867
ferdibytwer <ferdiuns24@gmail.com>
2026-02-14 11:18:05 +0700

    Perbaiki navigasi

--- src/compenents/Button.jsx
+++ src/compenents/Button.jsx
@@ -11,15 +11,15 @@ export default function Button({
     <Link
       href={href}
       aria-label={ariaLabel}
-      className={`relative z-999 overflow-hidden px-4 py-2 border bg-transparent group flex items-center justify-center ${className} ${
+      className={`relative z-10 overflow-hidden px-4 py-2 border bg-transparent group flex items-center justify-center transition-transform active:scale-95 ${className} ${
         disabled ? "pointer-events-none opacity-50" : ""
       }`}
     >
-      <span className="relative z-1 text-black group-hover:text-white transition-colors duration-300">
+      <span className="relative z-20 text-black md:group-hover:text-white transition-colors duration-300">
         {children}
       </span>
 
-      <span className="absolute inset-0 bg-black transform scale-0 origin-bottom-left group-hover:scale-100 transition-transform duration-300 z-0 pointer-events-none"></span>
+      <span className="absolute inset-0 bg-black transform scale-0 origin-bottom-left md:group-hover:scale-100 transition-transform duration-300 z-0 pointer-events-none"></span>
     </Link>
   );
 }
